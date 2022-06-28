65342ca7838c6e3da01089f4f85d0c99981efbed
Alef Silva <alef.dev.silva@gmail.com>
2022-06-28 16:04:36 -0300

    Criando projeto domain e projeto infra

---
+++ ContaBancaria.Web/Controllers/ContaBancariaController.cs
@@ -0,0 +1,24 @@
+﻿using ContaBancaria.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContaBancaria.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContaBancariaController
+    {
+        
+        Pessoa pessoa = new Pessoa(1, "Alef Silva");
+        [HttpGet("{pessoas}")]
+        public Pessoa RetornaNome()
+        {
+            return pessoa;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+    }
+}
---
+++ ContaBancaria.Web/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿namespace ContaBancaria.Web.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; } = string.Empty;
+        public string Endereco { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(int id, string nomeCompleto)
+        {
+            Id = id;
+            NomeCompleto = nomeCompleto;
+        }
+
+    }
+}
